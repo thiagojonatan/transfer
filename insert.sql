e6a3c8937c9d06cbcb487e63031bde8e6f356342
Thiago Jonatan <thiago.jonatan@gmail.com>
2023-11-11 18:52:17 -0300

    exemplo insert

---
+++ insert.sql
@@ -0,0 +1,69 @@
+INSERT INTO `USER`.user_domain
+(id, age, `data`, email, name)
+VALUES(23, '22', '1949-01-27 07:17:43', 'Caleb_Fox656@atink.com', 'Caleb Fox');
+INSERT INTO `USER`.user_domain
+(id, age, `data`, email, name)
+VALUES(22, '75', '1917-12-17 21:37:06', 'Angelique_Fleming9375@liret.org', 'Angelique Fleming');
+INSERT INTO `USER`.user_domain
+(id, age, `data`, email, name)
+VALUES(21, '30', '1844-11-03 22:50:50', 'Chris_Tailor2181@brety.org', 'Chris Tailor');
+INSERT INTO `USER`.user_domain
+(id, age, `data`, email, name)
+VALUES(20, '67', '1872-06-12 00:16:32', 'Sabrina_Cooper2163@famism.biz', 'Sabrina Cooper');
+INSERT INTO `USER`.user_domain
+(id, age, `data`, email, name)
+VALUES(19, '48', '1920-01-25 07:02:04', 'Ember_Garcia9747@extex.org', 'Ember Garcia');
+INSERT INTO `USER`.user_domain
+(id, age, `data`, email, name)
+VALUES(18, '93', '1830-07-31 17:49:11', 'Anais_Freeburn7990@bretoux.com', 'Anais Freeburn');
+INSERT INTO `USER`.user_domain
+(id, age, `data`, email, name)
+VALUES(17, '95', '1931-09-13 12:51:25', 'Selena_Baker996@tonsy.org', 'Selena Baker');
+INSERT INTO `USER`.user_domain
+(id, age, `data`, email, name)
+VALUES(16, '86', '1958-12-12 20:48:50', 'Aiden_Wilde1678@gembat.biz', 'Aiden Wilde');
+INSERT INTO `USER`.user_domain
+(id, age, `data`, email, name)
+VALUES(15, '51', '1932-10-22 08:33:09', 'Owen_Gavin7497@nanoff.biz', 'Owen Gavin');
+INSERT INTO `USER`.user_domain
+(id, age, `data`, email, name)
+VALUES(14, '64', '1955-04-12 19:51:02', 'Elle_Bennett2912@dionrab.com', 'Elle Bennett');
+INSERT INTO `USER`.user_domain
+(id, age, `data`, email, name)
+VALUES(13, '53', '2009-11-26 13:52:22', 'Rylee_Price3667@corti.com', 'Rylee Price');
+INSERT INTO `USER`.user_domain
+(id, age, `data`, email, name)
+VALUES(12, '36', '1927-06-26 04:33:21', 'Lily_Redwood4998@vetan.org', 'Lily Redwood');
+INSERT INTO `USER`.user_domain
+(id, age, `data`, email, name)
+VALUES(11, '33', '1842-12-18 12:58:31', 'Russel_Gilbert3516@sheye.org', 'Russel Gilbert');
+INSERT INTO `USER`.user_domain
+(id, age, `data`, email, name)
+VALUES(10, '92', '1948-10-17 01:01:22', 'Clint_Lloyd7713@ovock.tech', 'Clint Lloyd');
+INSERT INTO `USER`.user_domain
+(id, age, `data`, email, name)
+VALUES(9, '75', '1908-02-20 14:53:55', 'Deborah_Everett8241@nimogy.biz', 'Deborah Everett');
+INSERT INTO `USER`.user_domain
+(id, age, `data`, email, name)
+VALUES(8, '92', '1831-04-07 00:34:11', 'Ron_Lewin3299@deons.tech', 'Ron Lewin');
+INSERT INTO `USER`.user_domain
+(id, age, `data`, email, name)
+VALUES(7, '84', '1887-09-24 01:31:03', 'Luke_Martin468@muall.tech', 'Luke Martin');
+INSERT INTO `USER`.user_domain
+(id, age, `data`, email, name)
+VALUES(6, '79', '1928-12-21 08:21:24', 'Hank_Phillips6190@eirey.tech', 'Hank Phillips');
+INSERT INTO `USER`.user_domain
+(id, age, `data`, email, name)
+VALUES(5, '37', '2002-11-20 22:15:09', 'Chad_Pierce480@eirey.tech', 'Chad Pierce');
+INSERT INTO `USER`.user_domain
+(id, age, `data`, email, name)
+VALUES(4, '61', '1955-09-27 20:28:30', 'Anthony_Leigh1917@eirey.tech', 'Anthony Leigh');
+INSERT INTO `USER`.user_domain
+(id, age, `data`, email, name)
+VALUES(3, '56', '1843-09-27 07:22:03', 'Bernadette_Lee4410@hourpy.biz', 'Bernadette Lee');
+INSERT INTO `USER`.user_domain
+(id, age, `data`, email, name)
+VALUES(2, '29', '1887-06-15 03:10:50', 'Andrea_Bailey3082@corti.com', 'Andrea Bailey');
+INSERT INTO `USER`.user_domain
+(id, age, `data`, email, name)
+VALUES(1, '41', '2000-05-12 18:26:30', 'Kieth_Clark2321@zorer.org', 'Kieth Clark');
